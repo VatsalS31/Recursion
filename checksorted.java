75b30869da72013c61c4957cf225e04dbdffbe95
VatsalS31 <120210435+VatsalS31@users.noreply.github.com>
2022-12-11 12:27:32 +0530

    checksorted

---
+++ checksorted.java
@@ -0,0 +1,34 @@
+public class checksorted {
+
+	
+	public static boolean checkSorted(int input[]){
+		
+		if(input.length <= 1){
+			return true;
+		}
+		
+		int smallInput[] = new int[input.length - 1];
+		for(int i = 1; i < input.length; i++){
+			smallInput[i - 1] = input[i];
+		}
+		
+		boolean smallAns = checkSorted(smallInput);
+		if(!smallAns){
+			return false;
+		}
+		if(input[0] <= input[1]){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+    public static void main(String[] args) {
+		int input[] = {5,9,4,1,2,3};
+		System.out.println(" ");
+		
+		System.out.println(checkSorted(input));
+		System.out.println(" ");
+
+	}
+}
